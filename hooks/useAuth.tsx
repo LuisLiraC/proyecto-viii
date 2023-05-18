4917fb2e070468fc7790d934ad54df3d8e2cb6e5
Luis Lira <luis_ssbm@hotmail.com>
2023-05-17 21:13:16 -0600

    sign in and sign up

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -3,26 +3,64 @@ import { useRouter } from "next/router";
 
 interface AuthContextData {
   isLogged: boolean;
-  signIn: () => Promise<void>;
+  signIn: (credentials: Credentials) => Promise<void>;
   signOut: () => Promise<void>;
+  signUp: (newUser: newUser) => Promise<void>;
 }
 
+type Credentials = {
+  email: string;
+  password: string;
+}
+
+type newUser = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function useAuthProvider() {
   const [isLogged, setIsLogged] = useState(false);
   const router = useRouter();
 
-  const signIn = async () => {
-    document.cookie = "token=true";
+  const signIn = async (credentials: Credentials) => {
+    const res = await fetch('/api/v1/auth/sign_in', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
+
+    const data = await res.json();
+
+    document.cookie = `token=${data.token}`;
     setIsLogged(true);
-    router.push("/profile");
+    await router.push("/profile");
+  };
+
+  const signUp = async (newUser: newUser) => {
+    const res = await fetch('/api/v1/auth/sign_up', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    const data = await res.json();
+
+    document.cookie = `token=${data.token}`;
+    setIsLogged(true);
+    await router.push("/profile");
   };
 
   const signOut = async () => {
     document.cookie = "token=false; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     setIsLogged(false);
-    router.push("/");
+    await router.push("/");
   };
 
   useEffect(() => {
@@ -34,7 +72,8 @@ function useAuthProvider() {
   return {
     isLogged,
     signIn,
-    signOut
+    signOut,
+    signUp
   };
 }
 
--- pages/api/v1/auth/sign_in.ts
+++ pages/api/v1/auth/sign_in.ts
@@ -17,9 +17,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const userProfileRepository = new PostgresUserProfileRepository(postgres);
 
   const user = await authUserRepository.findByEmail(email);
+
+  if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+  
   const passwordMatch = await bcrypt.compare(password, user.password);
 
-  if (!user || !passwordMatch) return res.status(400).json({ message: 'Invalid credentials' });
+  if (!passwordMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
   const userProfile = await userProfileRepository.findById(user.id);
 
